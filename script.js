658f0a40266735d573c1e1d699be44b1bd9920a2
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-11-09 10:02:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function onesDigit(n) {
   // your code here
+	if(n <= 0) return "Not Valid";
+	else return n%10;
+	
 }
 
 // Do not change the code below
 
-const n = prompt("Enter Number:");
+const n = parseInt(prompt("Enter Number:"));
 alert(onesDigit(n));
